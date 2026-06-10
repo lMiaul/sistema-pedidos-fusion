ecd605e9d3b1ae8316483de98fcb2c78050d6828
Mari Cieza <106277521+MARI2002SOL@users.noreply.github.com>
2026-06-10 01:00:52 +0000

    Regreso al api a la normalidad

--- 2-backend-api/main.py
+++ 2-backend-api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 import os
 from datetime import datetime
 
@@ -23,21 +23,11 @@ client = MongoClient(MONGO_URL)
 db = client["restaurante_fusion"]
 ordenes_collection = db["ordenes"]
 
-# Definimos cómo se ve un plato dentro de la lista
-class Plato(BaseModel):
-    nombre: str
-    categoria: str
-    precio: float
-    cantidad: int
-    nota: Optional[str] = None
-
-# Actualizamos el modelo Orden
+# Modelo de datos para validar la entrada de pedidos
 class Orden(BaseModel):
     mesa: int
-    turno: str
-    mesero: str
-    estado: str
-    platos: List[Plato] # Ahora recibe la lista de objetos complejos
+    platos: List[str]
+    estado: str = "En cola"  # Estados: En cola, Preparando, Listo
 
 @app.get("/")
 def read_root():
